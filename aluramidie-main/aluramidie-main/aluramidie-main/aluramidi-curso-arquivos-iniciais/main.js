0700efb369e0dafc3755279a6cba7966b2e6f776
luis2029h <106539628+luis2029h@users.noreply.github.com>
2023-10-11 09:53:31 -0300

    Add files via upload

--- aluramidie-main/aluramidie-main/aluramidie-main/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidie-main/aluramidie-main/aluramidie-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,17 +1,1 @@
- function tocaSom(idElementoAudio) {
-   document.querySelector('#som_tecla_pom'). Play();
-   document.querySelector(idElementoAudio).Play();
-    document.querySelector('#som_tecla_pom'). onclick = TocaSomPomp; play ();
-}
-const listadeteclas = document.querySelectorA11('.tecla')
- listadeteclas[0].onclick= tocasomPom;
- // Estrutura de repetição -  Enquato
- let contador = 0;
- while  (contador<8){}
- while   ( contador<9) {
-   listaDeTeclas[contador].onclick = tocaSom;
-    listadeteclas[0].onclick=tocasomPom;
-    contador = contador + 1;
-    console.log (contador);
-    while(contador<listadeteclas.length);
- }
+alert('Olá, mundo');
---
+++ aluramidie-main/aluramidie-main/aluramidie-main/aluramidie-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,25 @@
+ function tocaSom(idElementoAudio) {
+   document.querySelector('#som_tecla_pom'). Play();
+   document.querySelector(idElementoAudio).Play();
+    document.querySelector('#som_tecla_pom'). onclick = TocaSomPomp; play ();
+}
+const listadeteclas = document.querySelectorA11('.tecla')
+ listadeteclas[0].onclick= tocasomPom;
+ // Estrutura de repetição -  Enquato
+ let contador = 0;
+ while  (contador<8){}
+ while   ( contador<9) {
+   listaDeTeclas[contador].onclick = tocaSom;
+    listadeteclas[0].onclick=tocasomPom;
+    contador = contador + 1;
+    console.log (contador);
+    while(contador<listadeteclas.length);
+    listaDeTeclas[contador].classList[1];
+ }
+ const tecla = listaDeTeclas[contador];
+ const instrumento =listadeteclas[contador].classList[1]
+      const Idaudio ='#som_ $ {instrumento}';
+ console.log(instrumento);
+  listadetecla [contador]. onclick= onclick = function() {
+tocaSom (idaudio)
+  };
