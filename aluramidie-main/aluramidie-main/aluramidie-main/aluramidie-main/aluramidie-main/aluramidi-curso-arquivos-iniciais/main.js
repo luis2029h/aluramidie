149e85dd2e41d91ece66d57f95e4fb9863bb6ab4
luis2029h <106539628+luis2029h@users.noreply.github.com>
2023-10-25 09:48:10 -0300

    Add files via upload

---
+++ aluramidie-main/aluramidie-main/aluramidie-main/aluramidie-main/aluramidie-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,30 @@
+ function tocaSom(idElementoAudio) {
+   document.querySelector('#som_tecla_pom'). Play();
+   document.querySelector(idElementoAudio).Play();
+    document.querySelector('#som_tecla_pom'). onclick = TocaSomPomp; play ();
+}
+const listadeteclas = document.querySelectorA11('.tecla')
+ listadeteclas[0].onclick= tocasomPom;
+ // Estrutura de repetição -  Enquato
+ let contador = 0;
+ while  (contador<8){}
+ while   ( contador<9) {
+   listaDeTeclas[contador].onclick = tocaSom;
+    listadeteclas[0].onclick=tocasomPom;
+    contador = contador + 1;
+    console.log (contador);
+    for (let contador = 0; < listadeteclas.length; contador++){
+    }
+    listaDeTeclas[contador].classList[1];
+ }
+ const tecla = listaDeTeclas[contador];
+ const instrumento =listadeteclas[contador].classList[1]
+      const Idaudio ='#som_ $ {instrumento}';
+      tecla.onclick = function () { 
+        tocaSom('#toca_som_splash') ;
+        tocaSom(Audio);
+      }
+ console.log(instrumento);
+  listadetecla [contador]. onclick= onclick = function() {
+tocaSom (idaudio)
+  };
